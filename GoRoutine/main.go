5a58c5197f707caa2cf7eb742190a5caa9992055
panhq <hypofiasco@gmail.com>
2022-06-10 17:52:54 +0800

    sync

--- GoRoutine/main.go
+++ GoRoutine/main.go
@@ -2,17 +2,72 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sync"
+	"time"
 )
 
-func say(s string) {
-	runtime.Gosched()
-	for i := 0; i < 5; i++ {
-		fmt.Println(s)
+var x int64
+
+var waitGroup sync.WaitGroup
+
+var lock sync.Mutex
+
+//var rwLock sync.RWMutex
+
+func write() {
+	lock.Lock()
+	x = x + 1
+	time.Sleep(2 * time.Second)
+	lock.Unlock()
+	waitGroup.Done()
+}
+
+func read() {
+	lock.Lock()
+	time.Sleep(time.Second)
+	lock.Unlock()
+	waitGroup.Done()
+}
+
+//func main() {
+// waitGroup、lock
+//start := time.Now()
+//waitGroup.Add(200)
+//for i := 0; i < 100; i++ {
+//	go write()
+//}
+//
+//for i := 0; i < 100; i++ {
+//	go read()
+//}
+//
+//waitGroup.Wait()
+//
+//fmt.Println(time.Now().Sub(start))
+//}
+
+var icons map[string]string
+
+var initializeOnce = sync.Once{}
+
+func initializeIcons() {
+	icons = map[string]string{
+		"left":  "OK1",
+		"up":    "OK2",
+		"right": "OK3",
+		"down":  "OK4",
 	}
 }
 
+func Icon(name string) string {
+	initializeOnce.Do(initializeIcons)
+	return icons[name]
+}
+
+var syncMap = sync.Map{}
+
 func main() {
-	go say("world")
-	say("hello")
+	fmt.Println(Icon("left"))
+	syncMap.Store("a", "b")
+	fmt.Println(syncMap.Load("a"))
 }
